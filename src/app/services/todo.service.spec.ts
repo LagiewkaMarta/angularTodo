0d3d06e73a0154ac37495a8e9b05d1e5318ef0de
Marta <marta.lagiewka@skygate.io>
2019-10-24 10:54:41 +0200

    Todo list continued

--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -3,10 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: TodoService = TestBed.get(TodoService);
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		const service: TodoService = TestBed.get(TodoService);
+		expect(service).toBeTruthy();
+	});
 });
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { Todo } from '../models/Todo';
+const httpOptions = {
+	headers: new HttpHeaders({
+		'Content-Type': 'application/json'
+	})
+};
 @Injectable({
 	providedIn: 'root'
 })
 export class TodoService {
-	constructor() {}
+	todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+	todosLimit = '?_limit=5';
+	constructor(private http: HttpClient) {}
 
-	getTodos() {
-		return [
-			{
-				id: 1,
-				title: 'todo 1',
-				completed: false
-			},
-			{
-				id: 2,
-				title: 'todo 2',
-				completed: true
-			},
-			{
-				id: 3,
-				title: 'todo 3',
-				completed: false
-			}
-		];
+	getTodos(): Observable<Todo[]> {
+		return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+	}
+
+	//toggle todos - PUT request to update on the server
+	toggleCompleted(todo: Todo): Observable<any> {
+		const url = `${this.todosUrl}/${todo.id}`;
+		return this.http.put(url, todo, httpOptions);
 	}
 }
